7935a934c976585b858a4d6aed7d180d8bd353ff
Benjamin GIRAUD-RENARD <bgiraud@protonmail.com>
2023-11-27 22:25:40 +0100

    add default secret

---
+++ project/include/secrets.h
@@ -0,0 +1,12 @@
+#ifndef secrets
+#define secrets
+
+// WIFI CONFIGURATION
+#define SECRET_SSID "SSID"
+#define SECRET_PASS "PASSWORD"
+
+// MQTT CONFIGURATION
+#define MQTT_SERVER "192.168.0.0"
+#define MQTT_PORT 1883
+
+#endif
