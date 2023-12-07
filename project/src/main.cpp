ba603a27170b54a1cba2d1fa6dcb1c07915b3a27
Benjamin GIRAUD-RENARD <bgiraud@protonmail.com>
2023-12-07 18:59:52 +0100

    bluetooth and wifi interconnected but quite random

--- project/src/main.cpp
+++ project/src/main.cpp
@@ -8,106 +8,163 @@
 #include <BLEUtils.h>
 #include <BLEScan.h>
 #include <BLEAdvertisedDevice.h>
+#include "secrets.h"
 
+int scanTime = 5; // durée du scan en secondes
+BLEScan *pBLEScan;
 
-WiFiClient wifiClient; 
-PubSubClient client(wifiClient); 
-
-#define TARGET_DEVICE "E0:6D:17:50:9F:15"
-
-const char * ssid = "WIFI_TP_IOT";
-const char * password = "TP_IOT_2022";
-int scanTime = 5; //In seconds
+#define MQTT_TOPIC "IOT"
 
 uint32_t now;
-IPAddress server(192, 168, 31, 134);
 
-class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
-    void onResult(BLEAdvertisedDevice advertisedDevice) { 
-        Serial.println("TRYING TO GET RSSI VALUE"); 
-        int rssi = advertisedDevice.getRSSI(); 
-        if(rssi > (- 67)){
-            //We have one result but we don't know what it is 
-            Serial.println("Device name : "); 
-            Serial.println(advertisedDevice.getAddress().toString().c_str()); 
-            Serial.print("Rssi: ");
-            Serial.println(rssi); 
+WiFiClient wifiClient;
+PubSubClient client(wifiClient);
+TFT_eSprite tftSprite = TFT_eSprite(&M5.Lcd);
+IPAddress server; // IP address of the MQTT server
+
+const char *ssid = SECRET_SSID;     // name of the wifi network
+const char *password = SECRET_PASS; // password of the wifi network
+bool addr = server.fromString(MQTT_SERVER);
+const int port = MQTT_PORT; // port of the MQTT server
+String mqttGlobalTopic = MQTT_TOPIC;
+
+long lastReconnectAttempt = 0;
+String macAddress = "";
+const int ledPin = 26;
+bool state = false;
+bool draw = false;
+
+bool deviceFound = false;
+int rssiValue;
+
+bool bleConnected;
+
+class MyAdvertisedDeviceCallbacks : public BLEAdvertisedDeviceCallbacks
+{
+    void onResult(BLEAdvertisedDevice advertisedDevice)
+    {
+        if (advertisedDevice.getName() == "M5Stack")
+        {
+            deviceFound = true;
+            rssiValue = advertisedDevice.getRSSI();
         }
     }
-}; 
+};
 
-/*    Serial.printf("Advertised Device: %s \n", advertisedDevice.toString().c_str());
-        if(advertisedDevice.getAddress().equals(BLEAddress(TARGET_DEVICE))){
-            Serial.print("RSSI Value : "); 
-            //Serial.println(advertisedDevice.getRSSI()); 
-        }
-*/
-
-void callback(char * topic, byte * payload, unsigned int length){
-    DynamicJsonDocument jsonDynamic(1024); 
-
-    Serial.print("Message recu : "); 
-    Serial.println(topic); 
-/*     deserializeJson(jsonDynamic, (char*)(payload));
-    bool find = false;
-    for (int i = 0; i < sensors.size(); i++) {
-        if(sensors[i].getMac() == (jsonDynamic["mac"])) {
-            sensors[i].setHum(jsonDynamic["humidity"]);
-            sensors[i].setPressure(jsonDynamic["pressure"]);
-            sensors[i].setTemp(jsonDynamic["temperature"]);
-            find = true;
-            break;
-        }
-    }
-    if(!find) sensors.push_back(EnvSensor(jsonDynamic["mac"], jsonDynamic["temperature"], jsonDynamic["humidity"], jsonDynamic["pressure"])); */ 
-    Serial.println(); 
+void drawScreen()
+{
+    tftSprite.fillScreen(TFT_BLACK);
+    tftSprite.setTextColor(TFT_WHITE);
+    tftSprite.setFont(&FreeMonoBold9pt7b);
+    tftSprite.setTextDatum(MC_DATUM);
+    tftSprite.drawString(String("MAC: ") + macAddress.c_str(), 0, 10);
+    tftSprite.drawString(String("Wifi Connection: ") + (WiFi.isConnected() ? "OK" : "ERROR"), 0, 30);
+    tftSprite.drawString(String("MQTT Connection: ") + (client.connected() ? "OK" : "ERROR"), 0, 50);
+    tftSprite.drawString(String("Output state: ") + (state ? "ON" : "OFF"), 0, 70);
+    tftSprite.pushSprite(0, 0);
 }
 
-/* void reconnect(){
-    while(!client.connected()){
-        if(!client.connect(WiFi.macAddress().c_str())){
-            Serial.println("Erreur : tentative de connexion"); 
-            delay(5000);
-        } else {
-            client.subscribe("Music/#"); 
-        }
+boolean reconnect()
+{
+    Serial.println("Connection to MQTT server at " + server.toString() + ":" + String(port));
+    if (client.connect(WiFi.macAddress().c_str()))
+    {
+        String subscribeTopic = String(mqttGlobalTopic) + "/" + macAddress + "/#";
+        Serial.println("M5Stack subscribed to the topic " + subscribeTopic);
+        client.subscribe(subscribeTopic.c_str());
     }
-} */
+    return client.connected();
+}
 
+void callback(char *topic, byte *payload, unsigned int length)
+{
+    if (String(topic) == mqttGlobalTopic + "/" + macAddress + "/action")
+    {
+        String action = String(payload, length);
+        Serial.println("Get action: " + action);
+        digitalWrite(ledPin, digitalRead(ledPin) == HIGH ? LOW : HIGH);
+        state = state ? false : true;
+        drawScreen();
+    }
+}
 
-void setup() {
+void setup()
+{
+    Serial.begin(115200);
     M5.begin();
-    M5.Power.begin(); 
-/*    WiFi.begin(ssid, password); 
-     while(WiFi.status() != WL_CONNECTED){
-        delay(500); 
-    } */
+    M5.Power.begin();
+
+    tftSprite.setColorDepth(8);
+    tftSprite.createSprite(320, 240);
+    tftSprite.fillScreen(TFT_BLACK);
+    tftSprite.setTextColor(TFT_WHITE);
+    tftSprite.setFont(&FreeMonoBold9pt7b);
+
     BLEDevice::init("");
-    BLEScan* pBLEScan = BLEDevice::getScan(); //create new scan
+    pBLEScan = BLEDevice::getScan(); // créer un nouveau scan
+    pBLEScan->setActiveScan(true);   // active scan utilise plus de puissance, mais obtient des résultats plus rapidement
+    pBLEScan->setInterval(100);
+    pBLEScan->setWindow(99); // moins d'intervalles minimise la probabilité de collision
     pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
-    pBLEScan->setActiveScan(false); //active scan uses more power, but get results faster
-    BLEScanResults foundDevices = pBLEScan->start(scanTime);
-/*     Serial.print("Devices found: ");
-    Serial.println(foundDevices.getCount());
-    Serial.println("Scan done!"); 
-    client.setServer(server, 1883);
-    client.setCallback(callback); */
-    now = millis();
-    Serial.println("Fin du Setup");
+
+    client.setServer(server, port);
+    client.setCallback(callback);
+
+    bleConnected = true;
+
+    drawScreen();
 }
 
+void loop()
+{
+    // lancer un nouveau scan
+    BLEScanResults foundDevices = pBLEScan->start(scanTime, false);
+    Serial.print("Devices found: ");
+    Serial.println(foundDevices.getCount());
+    Serial.println("Scan done!");
 
+    // if(advertisedDevice.getAddress().toString() == BLE_MAC_ADDRESS) {
+    if (deviceFound)
+    {
+        // set connection to wifi
 
-void loop() {
-    if(millis() - now > 1000){
-        now = millis();
+        BLEDevice::deinit();
+        bleConnected = false;
+
+        WiFi.begin(ssid, password);
+        WiFi.setAutoReconnect(true);
+        while (WiFi.status() != WL_CONNECTED)
+        {
+            tftSprite.fillScreen(TFT_BLACK);
+            tftSprite.drawString(String("Connection to WiFi: ") + ssid, 0, 10);
+            tftSprite.pushSprite(0, 0);
+            delay(500);
+        }
+
+        Serial.println("Wifi connected to address: " + WiFi.localIP().toString());
+        macAddress = WiFi.macAddress();
+        reconnect();
+
+        // todo : try to connect to /action callback
+
+        String mqttTopic = mqttGlobalTopic + String("/") + WiFi.macAddress();
+        client.publish(mqttTopic.c_str(), std::to_string(rssiValue).c_str());
+        Serial.println(String(rssiValue) + " envoyé au serveur ! " + mqttTopic);
+        delay(1000);
+        WiFi.disconnect(true, true);
+        delay(200);
+    }
+
+    //   delay(2000); // attendre avant de scanner à nouveau
+    if (!bleConnected)
+    {
+        BLEDevice::init("");
+        bleConnected = true;
+        deviceFound = false;
+        pBLEScan = BLEDevice::getScan(); // créer un nouveau scan
+        pBLEScan->setActiveScan(true);   // active scan utilise plus de puissance, mais obtient des résultats plus rapidement
+        pBLEScan->setInterval(100);
+        pBLEScan->setWindow(99); // moins d'intervalles minimise la probabilité de collision
+        pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
     }
 }
-
-
-
-
-
-
-
-
--- project/src/mqttConnection.cpp
+++ project/src/mqttConnection.cpp
@@ -1,137 +1,156 @@
-#include <M5Stack.h>
-#include "M5_ENV.h"
-#include <WiFi.h>
-#include <PubSubClient.h>
-#include <ArduinoJson.h>
-#include <vector>
-#include <BLEDevice.h>
-#include <BLEUtils.h>
-#include <BLEScan.h>
-#include <BLEAdvertisedDevice.h>
-#include "secrets.h"
+// #include <M5Stack.h>
+// #include "M5_ENV.h"
+// #include <WiFi.h>
+// #include <PubSubClient.h>
+// #include <ArduinoJson.h>
+// #include <vector>
+// #include <BLEDevice.h>
+// #include <BLEUtils.h>
+// #include <BLEScan.h>
+// #include <BLEAdvertisedDevice.h>
+// #include "secrets.h"
 
-#define MQTT_TOPIC "IOT"
+// #define MQTT_TOPIC "IOT"
 
-uint32_t now;
+// uint32_t now;
 
-WiFiClient wifiClient; 
-PubSubClient client(wifiClient);
-TFT_eSprite tftSprite = TFT_eSprite(&M5.Lcd);
-IPAddress server; // IP address of the MQTT server
+// WiFiClient wifiClient; 
+// PubSubClient client(wifiClient);
+// TFT_eSprite tftSprite = TFT_eSprite(&M5.Lcd);
+// IPAddress server; // IP address of the MQTT server
 
-const char* ssid = SECRET_SSID; // name of the wifi network
-const char* password = SECRET_PASS; // password of the wifi network
-bool addr = server.fromString(MQTT_SERVER);
-const int port = MQTT_PORT; // port of the MQTT server
-String mqttGlobalTopic = MQTT_TOPIC;
+// const char* ssid = SECRET_SSID; // name of the wifi network
+// const char* password = SECRET_PASS; // password of the wifi network
+// bool addr = server.fromString(MQTT_SERVER);
+// const int port = MQTT_PORT; // port of the MQTT server
+// String mqttGlobalTopic = MQTT_TOPIC;
 
-long lastReconnectAttempt = 0;
-String macAddress = "";
-const int ledPin = 26;
-bool state = false;
-bool draw = false;
+// long lastReconnectAttempt = 0;
+// String macAddress = "";
+// const int ledPin = 26;
+// bool state = false;
+// bool draw = false;
 
-void drawScreen() {
-    tftSprite.fillScreen(TFT_BLACK);
-    tftSprite.setTextColor(TFT_WHITE);
-    tftSprite.setFont(&FreeMonoBold9pt7b);
-    tftSprite.setTextDatum(MC_DATUM);
-    tftSprite.drawString(String("MAC: ") + macAddress.c_str(), 0, 10);
-    tftSprite.drawString(String("Wifi Connection: ") + (WiFi.isConnected() ? "OK" : "ERROR"), 0, 30);
-    tftSprite.drawString(String("MQTT Connection: ") + (client.connected() ? "OK" : "ERROR"), 0, 50);
-    tftSprite.drawString(String("Output state: ") + (state? "ON" : "OFF"), 0, 70);
-    tftSprite.pushSprite(0,0);
-}
+// class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
+//     void onResult(BLEAdvertisedDevice advertisedDevice) {
+// 		Serial.println();
+//       Serial.printf("Advertised Device: %s \n", advertisedDevice.toString().c_str());
+// 	  Serial.println("Tx Power: " + advertisedDevice.getTXPower());
+// 	  Serial.printf("Name: %s \n", advertisedDevice.getName());
+// 	  Serial.println();
+//     }
+// };
 
-void callback(char* topic, byte* payload, unsigned int length) {
-    if(String(topic) == mqttGlobalTopic+"/"+macAddress+"/action") {
-        String action = String(payload, length);
-        Serial.println("Get action: " + action);
-        digitalWrite(ledPin, digitalRead(ledPin) == HIGH? LOW : HIGH);
-        state = state? false : true;
-        drawScreen();
-    }
-}
+// void drawScreen() {
+//     tftSprite.fillScreen(TFT_BLACK);
+//     tftSprite.setTextColor(TFT_WHITE);
+//     tftSprite.setFont(&FreeMonoBold9pt7b);
+//     tftSprite.setTextDatum(MC_DATUM);
+//     tftSprite.drawString(String("MAC: ") + macAddress.c_str(), 0, 10);
+//     tftSprite.drawString(String("Wifi Connection: ") + (WiFi.isConnected() ? "OK" : "ERROR"), 0, 30);
+//     tftSprite.drawString(String("MQTT Connection: ") + (client.connected() ? "OK" : "ERROR"), 0, 50);
+//     tftSprite.drawString(String("Output state: ") + (state? "ON" : "OFF"), 0, 70);
+//     tftSprite.pushSprite(0,0);
+// }
 
-boolean reconnect(){
-    Serial.println("Connection to MQTT server at " + server.toString() + ":" + String(port));
-    if(client.connect(WiFi.macAddress().c_str())) {
-        String subscribeTopic = String(mqttGlobalTopic)+"/"+macAddress+"/#";
-        Serial.println("M5Stack subscribed to the topic " + subscribeTopic);
-        client.subscribe(subscribeTopic.c_str());
-    }
-    return client.connected();
-}
+// void callback(char* topic, byte* payload, unsigned int length) {
+//     if(String(topic) == mqttGlobalTopic+"/"+macAddress+"/action") {
+//         String action = String(payload, length);
+//         Serial.println("Get action: " + action);
+//         digitalWrite(ledPin, digitalRead(ledPin) == HIGH? LOW : HIGH);
+//         state = state? false : true;
+//         drawScreen();
+//     }
+// }
 
-void setup() {
-	M5.begin();
-	M5.Power.begin();
-    now = millis();
+// boolean reconnect(){
+//     Serial.println("Connection to MQTT server at " + server.toString() + ":" + String(port));
+//     if(client.connect(WiFi.macAddress().c_str())) {
+//         String subscribeTopic = String(mqttGlobalTopic)+"/"+macAddress+"/#";
+//         Serial.println("M5Stack subscribed to the topic " + subscribeTopic);
+//         client.subscribe(subscribeTopic.c_str());
+//     }
+//     return client.connected();
+// }
 
-    tftSprite.setColorDepth(8);
-    tftSprite.createSprite(320, 240);
-    tftSprite.fillScreen(TFT_BLACK);
-    tftSprite.setTextColor(TFT_WHITE);
-    tftSprite.setFont(&FreeMonoBold9pt7b);
+// void setup() {
+// 	M5.begin();
+// 	M5.Power.begin();
+//     now = millis();
 
-    WiFi.begin(ssid, password);
-    WiFi.setAutoReconnect(true);
-    int i = 0;
-    while (WiFi.status()!= WL_CONNECTED) {
-        tftSprite.fillScreen(TFT_BLACK);
-        tftSprite.drawString(String("Connection to WiFi: ") + ssid, 0, 10);
-        i++;
-        Serial.print(".");
-        tftSprite.drawString(std::string(i%3+1, '.').c_str(), 0, 30);
-        tftSprite.pushSprite(0,0);
+//     tftSprite.setColorDepth(8);
+//     tftSprite.createSprite(320, 240);
+//     tftSprite.fillScreen(TFT_BLACK);
+//     tftSprite.setTextColor(TFT_WHITE);
+//     tftSprite.setFont(&FreeMonoBold9pt7b);
 
-        delay(500);
-    }
+//     WiFi.begin(ssid, password);
+//     WiFi.setAutoReconnect(true);
+//     int i = 0;
+//     while (WiFi.status()!= WL_CONNECTED) {
+//         tftSprite.fillScreen(TFT_BLACK);
+//         tftSprite.drawString(String("Connection to WiFi: ") + ssid, 0, 10);
+//         i++;
+//         Serial.print(".");
+//         tftSprite.drawString(std::string(i%3+1, '.').c_str(), 0, 30);
+//         tftSprite.pushSprite(0,0);
 
-    Serial.println("Wifi connected to address: " + WiFi.localIP().toString());
-    macAddress = WiFi.macAddress();
+//         delay(500);
+//     }
+
+//     Serial.println("Wifi connected to address: " + WiFi.localIP().toString());
+//     macAddress = WiFi.macAddress();
     
-    client.setServer(server, port);
-    client.setCallback(callback);
+//     client.setServer(server, port);
+//     client.setCallback(callback);
     
-    pinMode(ledPin, OUTPUT);
-    digitalWrite(ledPin, LOW);
-    state = false;
-    draw = false;
+//     pinMode(ledPin, OUTPUT);
+//     digitalWrite(ledPin, LOW);
+//     state = false;
+//     draw = false;
 
-    drawScreen();
-    Serial.println("End setup"); 
-}
+//     drawScreen();
 
-void loop() {
-    if(!WiFi.isConnected()) {
-        now = millis();
-        if (now - lastReconnectAttempt > 5000) {
-            Serial.println("WiFi lost. Reconnect...");
-            lastReconnectAttempt = now;
-            WiFi.reconnect();
-            drawScreen();
-        }
-    }
-    else if (!client.connected()) {
-        now = millis();
-        if (now - lastReconnectAttempt > 5000) {
-            lastReconnectAttempt = now;
-            // Attempt to reconnect
-            if (reconnect()) {
-                lastReconnectAttempt = 0;
-            }
-            drawScreen();
-            draw = true;
-        }
-    } else {
-        if(draw) {
-            draw = false;
-            drawScreen();
-        }
-        // Client connected
-        client.loop();
-        String mqttTopic = mqttGlobalTopic + String("/") + WiFi.macAddress();
-        // client.publish(mqttTopic.c_str(), "value");
-    }
-}
+// 	BLEDevice::init("");
+//     BLEScan* pBLEScan = BLEDevice::getScan(); //create new scan
+//     pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
+//     pBLEScan->setActiveScan(false); //active scan uses more power, but get results faster
+//     BLEScanResults foundDevices = pBLEScan->start(5);
+
+//     Serial.println("End setup"); 
+// }
+
+// void loop() {
+//     if(!WiFi.isConnected()) {
+//         now = millis();
+//         if (now - lastReconnectAttempt > 5000) {
+//             Serial.println("WiFi lost. Reconnect...");
+//             lastReconnectAttempt = now;
+//             WiFi.reconnect();
+//             drawScreen();
+//         }
+//     }
+//     else if (!client.connected()) {
+//         now = millis();
+//         if (now - lastReconnectAttempt > 5000) {
+//             lastReconnectAttempt = now;
+//             // Attempt to reconnect
+//             if (reconnect()) {
+//                 String mqttTopic = mqttGlobalTopic + String("/") + WiFi.macAddress();
+//                 client.publish(mqttTopic.c_str(), "M5Stack MQTT reconnected");
+//                 lastReconnectAttempt = 0;
+//             }
+//             drawScreen();
+//             draw = true;
+//         }
+//     } else {
+//         if(draw) {
+//             draw = false;
+//             drawScreen();
+//         }
+//         // Client connected
+//         client.loop();
+//         String mqttTopic = mqttGlobalTopic + String("/") + WiFi.macAddress();
+//         // client.publish(mqttTopic.c_str(), "value");
+//     }
+// }
